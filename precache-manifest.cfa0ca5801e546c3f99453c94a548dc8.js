638ea26c18f1d143aa8e7ae158e20fd1aa93cece
iemeka <mekstines@gmail.com>
2021-05-15 20:34:30 +0100

    Updates

---
+++ precache-manifest.cfa0ca5801e546c3f99453c94a548dc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7d9b0408b62b27dfc72de0ed100aa9",
+    "url": "/typing-speed/index.html"
+  },
+  {
+    "revision": "be22d8779e46c506903f",
+    "url": "/typing-speed/static/css/main.40f21f68.chunk.css"
+  },
+  {
+    "revision": "c028c983432a30377450",
+    "url": "/typing-speed/static/js/2.c9a2cac9.chunk.js"
+  },
+  {
+    "revision": "be22d8779e46c506903f",
+    "url": "/typing-speed/static/js/main.f6eba8d6.chunk.js"
+  },
+  {
+    "revision": "a4b85a6a17d0998cd21a",
+    "url": "/typing-speed/static/js/runtime~main.6ddb2547.js"
+  },
+  {
+    "revision": "4dd585951cfd8c9f78cf3241ec4bbb7d",
+    "url": "/typing-speed/static/media/kick.4dd58595.wav"
+  }
+]);
