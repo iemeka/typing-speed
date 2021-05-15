200f2e7e3b18b0b8c5e7f2c3c4082113a29a71e2
iemeka <mekstines@gmail.com>
2021-05-15 21:36:33 +0100

    sound and cleanup2

--- src/Accuracy.js
+++ src/Accuracy.js
@@ -1,35 +1,24 @@
-import React, { useCallback, useContext, useEffect, useState } from "react";
+import React, { useCallback, useContext } from "react";
 import { utilityContext } from "./utilities/utilityContext";
 import text from "./resources/content";
 import "./Accuracy.css";
 
 export default function Accuracy() {
   const { pressedKeys } = useContext(utilityContext);
-  const [numLetterFailed, setNumLetterFailed] = useState(0);
-  const [numLetterPassed, setNumLetterPassed] = useState(0);
 
-  const currentIndex = useCallback(
-    () => pressedKeys.length - 1,
-    [pressedKeys]
-  )();
-
-  useEffect(() => {
-    if (currentIndex > -1 && currentIndex < text.length) {
-      if (pressedKeys[currentIndex] === text[currentIndex]) {
-        setNumLetterPassed((passed) => passed + 1);
-      } else if (pressedKeys[currentIndex] !== text[currentIndex]) {
-        setNumLetterFailed((failed) => failed + 1);
+  const calculateAccuracy = useCallback(() => {
+    let lettersPassed = 0;
+    pressedKeys.forEach((key, index) => {
+      if (index < text.length && key === text[index]) {
+        lettersPassed += 1;
       }
-    }
-  }, [pressedKeys, currentIndex]);
+    });
+    return Math.round(((lettersPassed / pressedKeys.length) * 100));
+  },[pressedKeys]);
 
   return (
     <div className="accuracy-wrapper">
-      <p>Pass : {numLetterPassed} </p>
-      <p>Fail : {numLetterFailed}</p>
-      <p>
-        Accuracy: {pressedKeys.length > 0 ? (((numLetterPassed / pressedKeys.length) * 100)).toFixed(2) : 0}%
-      </p>
+      <p>Accuracy: {pressedKeys.length > 0 ? calculateAccuracy() : 0}%</p>
     </div>
   );
 }
