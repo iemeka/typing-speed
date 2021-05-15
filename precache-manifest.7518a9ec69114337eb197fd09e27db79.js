89df7f39aa7759ae7ead01de3071a6d5749cc6d1
iemeka <mekstines@gmail.com>
2021-05-15 20:55:18 +0100

    Updates

--- precache-manifest.7518a9ec69114337eb197fd09e27db79.js
+++ precache-manifest.7518a9ec69114337eb197fd09e27db79.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "e1d3802a4b2bbf96879d9472522d1be6",
+    "revision": "668c80c606894ffb1396eaa83ba3fc78",
     "url": "/typing-speed/index.html"
   },
   {
-    "revision": "5e624777ac822c87bdf9",
-    "url": "/typing-speed/static/css/main.6cc8a668.chunk.css"
+    "revision": "c4d36a0cff9586fc317c",
+    "url": "/typing-speed/static/css/main.761895b5.chunk.css"
   },
   {
     "revision": "c028c983432a30377450",
     "url": "/typing-speed/static/js/2.c9a2cac9.chunk.js"
   },
   {
-    "revision": "5e624777ac822c87bdf9",
+    "revision": "c4d36a0cff9586fc317c",
     "url": "/typing-speed/static/js/main.b2b74242.chunk.js"
   },
   {
